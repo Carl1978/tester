11890df8bf6c86fbcf7e1f68685039391080868d
Carl Phillips <carl.phillips@gmail.com>
2018-09-27 17:32:29 +0100

    First commit!

---
+++ components.js
@@ -0,0 +1,2 @@
+export var myButton = document.querySelector('#IDButton');
+export var myParagraph = document.querySelector('#IDParagraph');
---
+++ index.js
@@ -0,0 +1,8 @@
+import {myButton, myParagraph} from './components';
+
+document.write("Hello World</ br>");
+
+let x = 123;
+
+myButton.innerHTML = "Changed!";
+
